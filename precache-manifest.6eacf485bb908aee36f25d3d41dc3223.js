df3df0e89d352ca6415c6d58caf6c2f3e89b17d3
sebastian <filipsebastian@Sebastians-MacBook-Pro.local>
2020-02-19 09:59:54 +0200

    Updates

---
+++ precache-manifest.6eacf485bb908aee36f25d3d41dc3223.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1264ae27aa396a40bf9579f95fdf2c7b",
+    "url": "/monsers.inc/index.html"
+  },
+  {
+    "revision": "542515bd33ea82578fc3",
+    "url": "/monsers.inc/static/css/main.c9334651.chunk.css"
+  },
+  {
+    "revision": "5174b0a43c4449f2680c",
+    "url": "/monsers.inc/static/js/2.5205c065.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsers.inc/static/js/2.5205c065.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "542515bd33ea82578fc3",
+    "url": "/monsers.inc/static/js/main.ff46d187.chunk.js"
+  },
+  {
+    "revision": "9ec2a0cdd47abc7e6291",
+    "url": "/monsers.inc/static/js/runtime-main.8180fd39.js"
+  }
+]);
